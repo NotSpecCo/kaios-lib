3000d724bf7ac35f4b1c15011193596f24c5f08f
Garrett Downs <garredow@outlook.com>
2022-03-25 08:47:45 -0400

    feat: add cicd

---
+++ release.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  branches: ['main'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    [
+      '@semantic-release/github',
+      {
+        successComment: false,
+        failComment: false,
+      },
+    ],
+    '@semantic-release/git',
+  ],
+};
