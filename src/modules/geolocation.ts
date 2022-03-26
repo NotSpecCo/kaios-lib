823b7c4365b9ff3072819d5d8fdddc350c5f19f4
Garrett Downs <garredow@outlook.com>
2022-03-26 05:56:34 -0400

    feat(geolocation): add subscribe method

--- src/modules/geolocation.ts
+++ src/modules/geolocation.ts
@@ -1,13 +1,13 @@
 import { Navigator } from './navigator';
 
-type GetCurrentOptions = {
+type Options = {
   maximumAge?: number;
   timeout?: number;
   enableHighAccuracy?: boolean;
 };
 
 export class Geolocation {
-  getCurrent(options?: GetCurrentOptions): Promise<Location> {
+  current(options?: Options): Promise<Location> {
     return new Promise((resolve, reject) => {
       Navigator.navigator.geolocation.getCurrentPosition(
         (position: Location) => resolve(position),
@@ -15,5 +15,15 @@ export class Geolocation {
         options
       );
     });
+  }
+
+  subscribe(
+    success: (data: Location) => void,
+    error?: (err: Error) => void,
+    options?: Options
+  ): () => void {
+    const id = Navigator.navigator.geolocation.watchPosition(success, error, options);
+    const unsubscribe = () => Navigator.navigator.geolocation.clearWatch(id);
+    return unsubscribe;
   }
 }
